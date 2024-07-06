807fcbaf2fda1d04a9ec9a8feccc96c1a3557324
ekrsw <eisuke.koresawa@gmail.com>
2024-07-07 07:39:47 +0900

    add dashboard

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig', # 追加
+    'dashboard.apps.DashboardConfig', # 追加
 ]
 
 MIDDLEWARE = [
