e37648ddd917404129ee2fda745cab3457602dd6
ekrsw <eisuke.koresawa@gmail.com>
2024-07-08 12:13:42 +0900

    add prod

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fm#*&o+q5=)^pne$2p4u0#lu!))be)d)+e_wt17-5m_^uz5$ek'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == "True"
